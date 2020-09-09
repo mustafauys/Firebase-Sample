0fa06759a51b86de505d36e5c0c122fb04a1be88
Mustafa <18701031@mersin.edu.tr>
2020-09-09 23:47:32 +0300

    Homepage File Added

---
+++ lib/homepage.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.red,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.pink,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
